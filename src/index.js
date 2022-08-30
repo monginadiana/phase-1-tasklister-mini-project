1db80381bd0c21d2d944560071fd97ca0d52ac6c
monginadiana <dianahmongina2@gmail.com>
2022-08-30 11:46:25 +0300

    addition of the even handlers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const newTaskDescription = document.getElementById("new-task-description");
+  const form = document.querySelector("form").addEventListener("submit", (e)=>{
+    e.preventDefault()
+    buildToDo(e.target.newTaskDescription)
+
+  })
+})
+function buildToDo(todo){
+  const newTaskDescription = document.getElementById("new-task-description");
+  let li = document.createElement("li")
+  let btn = document.createElement("button")
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  li.textContent = `${todo}`
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
